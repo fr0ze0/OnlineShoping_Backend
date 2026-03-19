c290f266a111105ee1933fa448373615164bef4c
fr0ze <themagdragon2@gmail.com>
2026-03-19 18:01:43 +0300

    added task2

--- src/Category.java
+++ src/Category.java
@@ -1,22 +1,26 @@
-public class Product {
+public abstract class Product {
     private int id;
     private String title;
     private double price;
+    private String description;
 
     public Product() {}
 
-    public Product(int id, String title, double price) {
-        this.id = id;
+    public Product(String title, String description, double price) {
         this.title = title;
+        this.description = description;
         this.price = price;
+        generateId();
+    }
+
+    private void generateId() {
+        String id_s = "";
+        this.title.chars().forEach(c -> id_s.join(String.valueOf(c)));
+        this.id = Integer.parseInt(id_s);
     }
 
     public int getId() {
         return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getTitle() {
---
+++ src/ElectronicItem.java
@@ -0,0 +1,5 @@
+import java.util.HashMap;
+
+public class Electronic extends Product {
+    public HashMap<Integer, String> products;
+}
