c531e6de5a1cbf49e9857a96f4ca23b3c9dde6cc
Valera <loxloxlox999lox@gmail.com>
2026-03-31 18:56:08 +0300

    helpme

--- src/GardenItem.java
+++ src/GardenItem.java
@@ -2,8 +2,7 @@ public class GardenItem extends Category {
     public GardenItem() {}
 
     public GardenItem(String title, String description, double price) {
-        this.setTitle(title);
-        this.setDescription(description);
+        this.setTitle(this.setDescription(description);
         this.setPrice(price);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -21,37 +21,9 @@ public class Main {
         System.out.println("Enter item count");
         count = scnr.nextInt();
 
-        for (int i = 0; i < count; i++) {
-            System.out.println("Choose category (1 - electronic, 2 - garden)");
-            int c = scnr.nextInt();
-            switch (c) {
-                case 1: {
-                    System.out.println("Enter title and price");
-                    String temp = scnr2.nextLine();
-                    String[] t_arr = temp.split(" ");
-                    temp_e_item.setTitle(t_arr[0]);
-                    temp_e_item.setPrice(Double.parseDouble(t_arr[1]));
-                    System.out.println("Enter description");
-                    temp_e_item.setDescription(scnr3.nextLine());
-                    items_e.add(temp_e_item);
-                }
-                case 2: {
-                    System.out.println("Enter title and price");
-                    String temp = scnr2.nextLine();
-                    String[] t_arr = temp.split(" ");
-                    temp_g_item.setTitle(t_arr[0]);
-                    temp_g_item.setPrice(Double.parseDouble(t_arr[1]));
-                    System.out.println("Enter description");
-                    temp_g_item.setDescription(scnr3.nextLine());
-                    items_g.add(temp_g_item);
-                }
-            }
-        }
-
         HashMap<Category, List<Category>> products = new HashMap<>();
 
         products.put(electronicCategory, items_e);
         products.put(gardenCategory, items_g);
-
+        }
     }
-}
